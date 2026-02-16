b55e13a179c298f32a978a22362c27a3ebbd3d43
Нурболат Бигельдиев <nurbolatbigeldiev@Nurbolats-MacBook-Air.local>
2026-02-16 23:12:15 +0500

    add all

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import PlainTextResponse
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Boss"}
 @app.get("/ping")
 def ping():
     return {"ping": "pong!"}
 
-@app.get("/info")
-def info(request: Request):
-    return {
-        "method": request.method,
-        "url": str(request.url),
-        "headers": dict(request.headers)
-    }
+@app.get("/info", response_class=PlainTextResponse)
+async def info(request: Request):
+    return (
+        f"{request.method}\n"
+        f"{request.url}\n"
+        f"HTTP/{request.scope.get('http_version')}"
+    )
+@app.post("/meaning-of-life")
+def meaning_of_life():
+    return {"meaning": "42"}
